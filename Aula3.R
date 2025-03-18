1c865de9a49cb6088bb9a403d7650ca521985f58
mgoesm <matheusbitt@hotmail.com>
2025-03-17 21:32:11 -0300

    Add files via upload

--- Aula3.R
+++ Aula3.R
@@ -4,5 +4,28 @@ A <- 1+1
 
 #PACOTE tidyverse
 
-install.packages("tidyvrese")
+install.packages("tidyverse")
 library(tidyverse)
+
+# WDI - WORLD DEVOLEPMENT INDICATORS
+#install.packages("WDI")
+library(WDI)
+
+# vignettes = paginas com instrucoes dos pacotes
+WDIsearch('gdp')
+library(remotes)
+install_github('vincentarelbundock/WDI')
+# GDP (current US$)(NY.GDP.MKTP.CD)
+# Código: NY.GDP.MKTP.CD
+
+varpib <- WDIsearch("gdp")
+
+options(scipen = 999)
+
+dadospib <- WDI(country = 'all', indicator = 'NY.GDP.MKTP.CD')
+
+dadospib2023 <- WDI(country = 'all', indicator = 'NY.GDP.MKTP.CD', start = 2023, end = 2023)
+
+
+                
+                
